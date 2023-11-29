499f4cadf17100c475c8ec28e8238e4da9d576e7
강지우 <82877633+jiwooriver@users.noreply.github.com>
2023-11-29 12:41:21 +0900

    Add files via upload

---
+++ primitiveType.js
@@ -0,0 +1,24 @@
+// 객체 자료형
+let member = new Number(273);
+let string = new String('안녕하세요');
+let boolean = new Boolean(true);
+
+// 자료형을 출력합니다.
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+
+
+
+
+
+
+
+
+// 변수를 입력합니다.
+let string3 = '안녕하세요. 좋은아침입니다.';
+
+// 문자열 내부에 '아침' 이라는 문자열이 있는지 확인합니다.
+if (string.indexOf('아침') >= 0) {
+    console.log('좋은 아침이에요...!');
+}
---
+++ stringMistake.js
@@ -0,0 +1,6 @@
+// 변수를 선언합니다
+let string2 = 'abcdefg';
+
+// 출력합니다.
+string = string.toUpperCase();
+console.log(string2);
